ca4e47eb7555267d0a6ef0df8ad239abf14cd0a9
dentcode <driggynyc@gmail.com>
2024-02-14 18:05:49 -0800

    Added Inserts for Products entities

--- DML.sql
+++ DML.sql
@@ -9,23 +9,14 @@ https://canvas.oregonstate.edu/courses/1946034/pages/exploration-database-applic
 -- get all information from Vendors
 SELECT * FROM Vendors;
 
--- get all information from Products, with vendor name standing in for vendorID
-SELECT Products.productID, Vendors.name AS vendor, Products.productPrice, Products.productWeight, Products.productDescription, Products.productInStock, Products.productName
-FROM Products
-INNER JOIN Vendors
-ON Products.vendorID = Vendors.vendorID;
+-- get all information from Products
+SELECT * FROM Products;
 
--- get all information from Products, with vendor name standing in for vendorID
-SELECT Invoices.invoiceID, Vendors.name AS vendor, Invoices.InvoiceDate
-FROM Invoices
-INNER JOIN Vendors
-ON Invoices.vendorID = Vendors.vendorID;
+-- get all information from Invoices
+SELECT * FROM Invoices;
 
--- get all information from Products, with product name standing in for productID
-SELECT Invoice_Items.invoiceItemsID, Invoice_Items.invoiceID, Products.productNAME AS product, Invoice_Items.orderQuantity, Invoice_Items.unitPrice
-FROM Invoice_Items
-INNER JOIN Products
-ON Invoice_Items.productID = Products.productID;
+-- get all information from Invoice_Items
+SELECT * FROM Invoice_Items;
 
 -- get all information from customers
 SELECT * FROM Customers;
@@ -37,7 +28,23 @@ SELECT * FROM Sales;
 SELECT * FROM Menu_Items;
 
 --get all sales items
-SELECT * FROM Sales_Items
+SELECT * FROM Sales_Items;
+
+-- Insert into Vendors
+INSERT INTO Vendors (name, phone, email)
+VALUES (:nameInput, :phoneInput, :emailInput);
+
+-- Insert into Products
+INSERT INTO Products (vendorID, productPrice, productWeight, productDescription, productInStock, productName)
+VALUES (:vendorIDInput, :productPriceInput, :productWeightInput, :productDescriptionInput, :productInStockInput, :productNameInput);
+
+-- Insert into Invoices
+INSERT INTO Invoices (vendorID, invoiceDate)
+VALUES (:vendorIDInput, :invoiceDateInput);
+
+-- Insert into Invoice_Items
+INSERT INTO Invoice_Items (invoiceID, productID, orderQuantity, unitPrice)
+VALUES (:invoiceIDInput, :productIDInput, :orderQuantityInput, :unitPriceInput);
 
 -- Insert into customers/ new customer
 INSERT INTO Customers (name, email)
