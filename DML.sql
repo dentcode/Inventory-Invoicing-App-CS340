14a46a450d532151d44ecff6cbbde228ed5c0ea5
dentcode <driggynyc@gmail.com>
2024-02-17 07:56:41 -0800

    Reorganized DML.sql by entity

--- DML.sql
+++ DML.sql
@@ -6,62 +6,82 @@ Code Citations: ALL code sections within this file uses code from
 https://canvas.oregonstate.edu/courses/1946034/pages/exploration-database-application-design?module_item_id=23809325 as the skeleton code.
 */
 
+----------------------
+--Vendors
+----------------------
+
 -- get all information from Vendors
 SELECT * FROM Vendors;
-
--- get all information from Products
-SELECT * FROM Products;
-
--- get all information from Invoices
-SELECT * FROM Invoices;
-
--- get all information from Invoice_Items
-SELECT * FROM Invoice_Items;
-
--- get all information from customers
-SELECT * FROM Customers;
-
--- get all sales
-SELECT * FROM Sales;
-
--- get all menu itmes
-SELECT * FROM Menu_Items;
-
---get all sales items
-SELECT * FROM Sales_Items;
 
 -- Insert into Vendors
 INSERT INTO Vendors (name, phone, email)
 VALUES (:nameInput, :phoneInput, :emailInput);
 
+---------------------
+--Products
+---------------------
+
+-- get all information from Products
+SELECT * FROM Products;
+
 -- Insert into Products
 INSERT INTO Products (vendorID, productPrice, productWeight, productDescription, productInStock, productName)
 VALUES (:vendorIDInput, :productPriceInput, :productWeightInput, :productDescriptionInput, :productInStockInput, :productNameInput);
+---------------------
+--Invoices
+---------------------
+
+-- get all information from Invoices
+SELECT * FROM Invoices;
 
 -- Insert into Invoices
 INSERT INTO Invoices (vendorID, invoiceDate)
 VALUES (:vendorIDInput, :invoiceDateInput);
 
+--------------------
+--Invoice_Items
+--------------------
+
+-- get all information from Invoice_Items
+SELECT * FROM Invoice_Items;
+
 -- Insert into Invoice_Items
 INSERT INTO Invoice_Items (invoiceID, productID, orderQuantity, unitPrice)
 VALUES (:invoiceIDInput, :productIDInput, :orderQuantityInput, :unitPriceInput);
+
+
+---------------------
+--Customers
+---------------------
+
+-- get all information from customers
+SELECT * FROM Customers;
 
 -- Insert into customers/ new customer
 INSERT INTO Customers (name, email)
 VALUES (:nameInput, :emailInput);
 
+---------------------
+--Sales
+---------------------
+
+-- get all sales
+SELECT * FROM Sales;
+
 -- Insert into sales
 INSERT INTO Sales (customerID, date)
 VALUES (:customerIDInput, :dateInput);
 
--- Insert into Menu itmes
+---------------------
+--Menu_Items
+---------------------
+
+-- get all menu itmes
+SELECT * FROM Menu_Items;
+
+-- Insert into Menu items
 INSERT INTO Menu_Items (name, listedPrice, menuItmeQuanity)
 VALUES (:nameInput, :listedPriceInput, :menuItmeQuanityInput);
-
-
--- Insert into Sales Items
-INSERT INTO Sales_Items (menuItemID, orderQuanity, unitPrice)
-VALUES (:nameInput, :emailInput);
 
 -- look for menu item ID by name
 SELECT menuItemID FROM Menu_Items
@@ -78,3 +98,15 @@ WHERE name = :name_menu_item_from_dropdown_input
 -- Delete Menu Item by ID
 DELETE FROM Menu_Items
 WHERE menuItemID = :menuItemIDInput
+
+---------------------
+--Sales_Iems
+---------------------
+
+--get all sales items
+SELECT * FROM Sales_Items;
+
+-- Insert into Sales Items
+INSERT INTO Sales_Items (menuItemID, orderQuanity, unitPrice)
+VALUES (:nameInput, :emailInput);
+
