bf8442a1db32fc547d838350af22e6543cb1576b
dentcode <driggynyc@gmail.com>
2024-02-25 05:06:14 -0800

    Completed Step 0-4 of github guide

--- app.js
+++ app.js
@@ -33,10 +33,46 @@ app.get('/', function (req, res) {
 
     db.pool.query(query1, function (error, rows, fields) {
 
-        res.render('index', { data: rows });      // Note the call to render() and not send(). Using render() ensures the templating engine
+        res.render('vendor', { data: rows });      // Note the call to render() and not send(). Using render() ensures the templating engine
     })                           // will process this file, before sending the finished HTML to the client.
 });
+// app.js - ROUTES section
 
+app.post('/add-vendor-ajax', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Vendors (vendorID, name, phone, email) VALUES ('${data.vendorID}', '${data.name}', ${data.phone}, ${data.email})`;
+    db.pool.query(query1, function (error, rows, fields) {
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Vendors;`;
+            db.pool.query(query2, function (error, rows, fields) {
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
 /*
     LISTENER
 */
---
+++ public/js/add_vendor.js
@@ -0,0 +1,104 @@
+/*
+Students: Guibely De Aguinaga and Ignacio De La Cruz
+Assignment: CS340 Project
+Course: CS340 - Group 39
+Code Citations: ALL code sections within this file uses code from
+https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%205%20-%20Adding%20New%20Data
+as the skeleton code.
+*/
+
+// Get the objects we need to modify
+let addvendorForm = document.getElementById('add-vendor-form-ajax');
+
+// Modify the objects we need
+addvendorForm.addEventListener("submit", function (e) {
+
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputVendorName = document.getElementById("input-vname");
+    let inputVendorPhone = document.getElementById("input-vphone");
+    let inputVendorEmail = document.getElementById("input-vemail");
+
+
+    // Get the values from the form fields
+    let vendorNameValue = inputVendorName.value;
+    let vendorPhoneValue = inputVendorPhone.value;
+    let vendorEmailValue = inputVendorEmail.value;
+
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        vname: vendorNameValue,
+        vphone: vendorPhoneValue,
+        vemail: vendorEmailValue,
+    }
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-vendor-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputVendorName.value = '';
+            inputVendorPhone.value = '';
+            inputVendorEmail.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("people-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let firstNameCell = document.createElement("TD");
+    let lastNameCell = document.createElement("TD");
+    let homeworldCell = document.createElement("TD");
+    let ageCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.id;
+    firstNameCell.innerText = newRow.vname;
+    lastNameCell.innerText = newRow.vphone;
+    homeworldCell.innerText = newRow.homeworld;
+    ageCell.innerText = newRow.age;
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(firstNameCell);
+    row.appendChild(lastNameCell);
+    row.appendChild(homeworldCell);
+    row.appendChild(ageCell);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
